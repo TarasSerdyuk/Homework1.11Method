4f00efa21d17bd86aed85ef3610db0edc69cb7cb
Тарас <Acroyd1984@gmail.com>
2023-03-16 21:06:06 +0300

    помогите разобраться

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,63 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
 
-    }
-    public static void task1() {
         System.out.println("Задача 1");
 
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println(fullName);
-    }
-    public static void task2() {
+        int year = 2023;
+        checkYear();
+
         System.out.println("Задача 2");
 
-        String fullName = "Ivanov Ivan Ivanovich";
-        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
-    }
-    public static void task3() {
+        int clientOS = 0;
+        int clientDeviceYear = 2018;
+        getDeviceSoft();
+
         System.out.println("Задача 3");
 
-        String fullName = "Иванов Семён Семёнович";
-        fullName = fullName.replace("ё", "е");
-        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+        int deliveryDistance = 95;
+        countDaysDelivery();
     }
-}
+
+
+    public static void checkYear (int year) {
+            if ((year % 4 == 0 && year % 100 == 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+          System.out.println(year + "год не является високосным");
+        }
+    }
+
+       public static void getDeviceSoft (int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0) {
+            if (clientDeviceYear <= currentYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылк");
+            }
+        } else {
+            if (clientDeviceYear <= currentYear) {
+                System.out.println("Установите облегченную версию приложения для Androyd по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Androyd по ссылке");
+            }
+        }
+    }
+    public static void countDaysDelivery(int deliveryDistance) {
+        int deliveryDays = 1;
+
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+        } else {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        }
+    }
+   }
